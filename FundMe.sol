22c00fa89bf71c1bb32428ed4e4eb85775ed9227
gitsutos <gitsutos@yahoo.com>
2022-06-29 12:29:19 +0530

    making immutable variable

--- FundMe.sol
+++ FundMe.sol
@@ -13,7 +13,7 @@ contract FundMe {
     address[] public funders;
 
     // Could we make this constant?  /* hint: no! We should make it immutable! */
-    address public /* immutable */ i_owner;
+    address public immutable  i_owner;
     uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
     
     constructor() {
